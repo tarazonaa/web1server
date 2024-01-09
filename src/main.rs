696dc5e481f586211a19b0a8667eedd2a289f23c
Tarazonaa <andres.tara.so@gmail.com>
2024-01-09 17:48:22 -0600

    changed templating to Tera

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-use handlebars::{DirectorySourceOptions, Handlebars};
-use std::collections::HashMap;
+use serde_derive::Serialize;
+use tera::{Context, Tera};
+
+#[derive(Serialize)]
+struct Contact {
+    id: i32,
+    first: String,
+    last: String,
+    phone: String,
+    email: String,
+}
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -8,31 +17,46 @@ async fn index() -> impl Responder {
 }
 
 #[get("/contacts")]
-async fn contacts(hb: web::Data<Handlebars<'_>>) -> HttpResponse {
+async fn contacts(tera: web::Data<Tera>) -> HttpResponse {
     // Don't use JSON as data
-    let mut data = HashMap::new();
-    data.insert("project_name", "Contacts");
-    data.insert("body", "This is the body");
-    let body = hb.render("index", &data).unwrap();
+    let contacts = vec![
+        Contact {
+            id: 1,
+            first: "John".to_string(),
+            last: "Doe".to_string(),
+            phone: "555-1234".to_string(),
+            email: "asdsasda@aasd.com".to_string(),
+        },
+        Contact {
+            id: 2,
+            first: "Jane".to_string(),
+            last: "Doe".to_string(),
+            phone: "555-4321".to_string(),
+            email: "asdsasda@gmail.com".to_string(),
+        },
+        Contact {
+            id: 3,
+            first: "John".to_string(),
+            last: "Smith".to_string(),
+            phone: "555-9876".to_string(),
+            email: "asdsasd@gmail.com".to_string(),
+        },
+    ];
+    let mut data = Context::new();
+    data.insert("title", "Contacts");
+    data.insert("q", "this is the search term");
+    data.insert("contacts", &contacts);
+    let body = tera.render("index.html", &data).unwrap();
     HttpResponse::Ok().body(body)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let mut handlebars = Handlebars::new();
-    let options = DirectorySourceOptions {
-        tpl_extension: ".html".into(),
-        ..Default::default()
-    };
-    handlebars
-        .register_templates_directory("./static/", options)
-        .unwrap();
-
-    let handlebars_ref = web::Data::new(handlebars);
-
+    let tera = Tera::new("./static/**/*.html").unwrap();
+    let tera_ref = web::Data::new(tera);
     HttpServer::new(move || {
         App::new()
-            .app_data(handlebars_ref.clone())
+            .app_data(tera_ref.clone())
             .service(index)
             .service(contacts)
     })
